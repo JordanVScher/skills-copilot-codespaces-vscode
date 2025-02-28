62739d546034c4351d6afef62b07ee0568abd9cd
Jordan Victor Scher <jordanvscher@hotmail.com>
2025-02-28 12:54:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  var member = {
+    name: 'Member',
+    color: 'green',
+    skills: [
+      { name: 'HTML', value: 0.9 },
+      { name: 'CSS', value: 0.9 },
+      { name: 'JavaScript', value: 0.7 },
+      { name: 'C#', value: 0.5 },
+      { name: 'Java', value: 0.5 },
+      { name: 'Python', value: 0.5 }
+    ]
+  };
+  return member;
+}
